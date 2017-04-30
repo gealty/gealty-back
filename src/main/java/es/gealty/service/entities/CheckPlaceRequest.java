31365bc5c67ef29262fbb9afb9585d748294078f
Zexnasorutra <arturosanchez93@gmail.com>
2017-04-30 07:37:42 +0200

    CORS and services

--- src/main/java/es/gealty/service/entities/CheckPlaceRequest.java
+++ src/main/java/es/gealty/service/entities/CheckPlaceRequest.java
@@ -2,9 +2,36 @@ package es.gealty.service.entities;
 
 public class CheckPlaceRequest {
 	private String seed;
+	private String lat;
+	private String lon;
+	private boolean save;
+
+	public boolean isSave() {
+		return save;
+	}
+
+	public void setSave(boolean save) {
+		this.save = save;
+	}
 
 	public String getSeed() {
 		return seed;
+	}
+
+	public String getLat() {
+		return lat;
+	}
+
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+
+	public String getLon() {
+		return lon;
+	}
+
+	public void setLon(String lon) {
+		this.lon = lon;
 	}
 
 	public void setSeed(String seed) {
--- src/main/java/es/gealty/service/rest/CheckPlaceService.java
+++ src/main/java/es/gealty/service/rest/CheckPlaceService.java
@@ -1,10 +1,15 @@
 package es.gealty.service.rest;
 
+import java.sql.SQLException;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import es.gealty.bean.Seed;
+import es.gealty.dao.SeedDAO;
+import es.gealty.dao.SeedHome;
 import es.gealty.service.entities.CheckPlaceRequest;
 import es.gealty.service.entities.CheckPlaceResponse;
 
@@ -14,9 +19,17 @@ public class CheckPlaceService {
 	@POST
 	@Consumes("application/json")
 	@Produces("application/json")
-	public CheckPlaceResponse checkPlace(CheckPlaceRequest request) {
+	public CheckPlaceResponse checkPlace(CheckPlaceRequest request) throws SQLException {
 		CheckPlaceResponse response = new CheckPlaceResponse();
-
+//		Seed seed = new Seed();
+//		seed.setId(132);
+		
+//		SeedHome dao = new SeedHome();
+//		dao.persist(seed);
+//		SeedDAO dao  = new SeedDAO();
+//		
+//		dao.getBean();
+		
 		return response;
 
 	}
